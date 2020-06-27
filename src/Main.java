d814e68fa0e5453d6bb41f1fd00bfe54d01bb0b6
Nikolay <nikolai-vyalkov@yandex.ru>
2020-06-27 22:30:14 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean ticket = true;
+        int cost = 1000;
+        float percent = 0.02F;
+
+        float bonus = cost * percent;
+        System.out.println(bonus);
+
+    }
+}
