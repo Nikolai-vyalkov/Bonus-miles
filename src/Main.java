c181c0539871bbe4b5d14d0fa904756a1fda8a05
Nikolay <nikolai-vyalkov@yandex.ru>
2020-07-02 23:46:39 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         boolean ticket = true;
-        int cost = 1000;
-        int bonus = 20;
+        int  cost = 1000;
+        int mile  = 20;
 
 
-        float mile = cost / bonus;
-        System.out.println(mile);
+        int bonus = cost/mile;
+        System.out.println(bonus);
 
     }
 }
